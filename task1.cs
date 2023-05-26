03f396f82b757c2c21fa194a460a2110bc6ba4c3
Anna <happyz2104@gmail.com>
2023-05-26 20:24:14 +0300

    ДЗ1

---
+++ task1.cs
@@ -0,0 +1,53 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*Console.WriteLine("Введи первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1>number2)
+Console.WriteLine ($"Число {number1} больше числа {number2} ");
+if (number1<number2)
+Console.WriteLine ($"Число {number1} меньше числа {number2} ");
+if (number1==number2)
+Console.WriteLine ($"Число {number1} равно числу {number2} "); */
+
+
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*Console.WriteLine("Введи первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи третье число: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+int max = num1;
+if(num1>max) max = num1;
+if(num2>max) max = num2;
+if(num3>max) max = num3;
+
+
+Console.WriteLine ($"Максимальным числом из трёх является {max} "); */
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+/*Console.WriteLine("Введите число: ");
+int numb = int.Parse(Console.ReadLine()!);
+
+if (numb%2==0) {
+Console.WriteLine ($"Число {numb} является чётным");
+}
+else
+{
+  Console.WriteLine ($"Число {numb} не является чётным");  
+} */
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*Console.WriteLine("Введи число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int num1 = 2;
+
+while(num1 <= num){
+    Console.Write($"{num1} ");
+    num1 = num1 + 2;
+} */
